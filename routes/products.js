22a6d461fd9409e4b93c3e41f4b641c0679a8ed5
sawood14012 <msawood@redhat.com>
2021-03-06 02:09:32 +0530

    added index search

--- routes/products.js
+++ routes/products.js
@@ -9,15 +9,31 @@ const client = algoliasearch('9CQGAYW5CN', algolia_key)
 const index = client.initIndex('FAKE_FLIPCART')
 
 /* GET Products listing. */
-router.get('/api', function (req, res, next) {
+router.get('/api/re-populate', function (req, res, next) {
   getProducts()
   .then(
     function(result){
+      clearAll()
       updateIndex(result)
       res.send(result)
     }
   )
 });
+
+router.get('/api', function(req, res, next){
+   getAll().then(
+     function(hits){
+       res.send(hits)
+     }
+   )
+
+});
+
+async function clearAll(){
+ result = await index.clearObjects().then((res) => {
+  console.log(res);
+}).catch(error => console.log(error));
+}
 
 async function updateIndex(products) {
   result = await index.saveObjects(products, {
@@ -27,10 +43,29 @@ async function updateIndex(products) {
   return result;
 }
 
+
+async function getAll(){
+  result = await index.search().catch(error => console.log(error));
+  return result.hits;
+}
+
 async function getProducts() {
+  var setObjId = [];
   result = await fetch('https://fakestoreapi.com/products/');
   res = await result.json();
-  return res;
+  await res.forEach(element => {
+    var temp = {
+      "objectID": element.id,
+      "title": element.title,
+      "description": element.description,
+      "category": element.category,
+      "price": element.price,
+      "image": element.image
+    }
+    setObjId.push(temp)
+
+  });
+  return setObjId;
     
 }
 
